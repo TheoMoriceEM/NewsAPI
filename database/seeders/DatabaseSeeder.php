5f2c7d1c5a140f34967f3fbbef4673e4174a53ce
TheoMoriceEM <theo.morice5@gmail.com>
2023-03-22 17:20:52 +0100

    Created seeders for countries and languages

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,62 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Language;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
+    private array $sampleData = [
+        [
+            'name'     => 'Belgium',
+            'code'     => 'be',
+            'language' => ['nl'],
+        ],
+        [
+            'name'     => 'Canada',
+            'code'     => 'ca',
+            'language' => [
+                'en',
+                'fr',
+            ],
+        ],
+        [
+            'name'     => 'France',
+            'code'     => 'fr',
+            'language' => ['fr'],
+        ],
+        [
+            'name'     => 'Germany',
+            'code'     => 'de',
+            'language' => ['de'],
+        ],
+        [
+            'name'     => 'United kingdom',
+            'code'     => 'gb',
+            'language' => ['en'],
+        ],
+    ];
+
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        foreach ($this->sampleData as $country) {
+            $countryId = DB::table('countries')->insertGetId([
+                'name' => $country['name'],
+                'code' => $country['code'],
+            ]);
+            foreach ($country['language'] as $language) {
+                $language = Language::firstOrCreate([
+                    'language' => $language,
+                ]);
+                DB::table('country_language')->insert([
+                    'country_id'  => $countryId,
+                    'language_id' => $language->id,
+                ]);
+            }
+        }
     }
 }
