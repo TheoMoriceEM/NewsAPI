e3a62e23972692acc47d57aa5f9e8f1604f18d57
TheoMoriceEM <theo.morice5@gmail.com>
2023-03-22 17:47:26 +0100

    Created seeder for categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\Language;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -39,11 +40,32 @@ class DatabaseSeeder extends Seeder
         ],
     ];
 
+    private array $categories = [
+        'business',
+        'entertainment',
+        'environment',
+        'food',
+        'health',
+        'politics',
+        'science',
+        'sports',
+        'technology',
+        'top',
+        'tourism',
+        'world',
+    ];
+
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
+        foreach ($this->categories as $category) {
+            DB::table('categories')->insert([
+                'name' => $category,
+            ]);
+        }
+
         foreach ($this->sampleData as $country) {
             $countryId = DB::table('countries')->insertGetId([
                 'name' => $country['name'],
@@ -56,6 +78,12 @@ class DatabaseSeeder extends Seeder
                 DB::table('country_language')->insert([
                     'country_id'  => $countryId,
                     'language_id' => $language->id,
+                ]);
+            }
+            foreach (Category::all() as $category) {
+                DB::table('category_country')->insert([
+                    'category_id' => $category->id,
+                    'country_id'  => $countryId,
                 ]);
             }
         }
